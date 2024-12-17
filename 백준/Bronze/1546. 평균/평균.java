f7f3849a82c1d33b5f7a2248825e2672593e1544
jaksfj <64482590+jaksfj@users.noreply.github.com>
2024-12-17 19:50:56 +0900

    [Bronze I] Title: 평균, Time: 112 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,22 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in );
-        int n = scanner.nextInt();
-        int []numArr = new int[n];
-        int max=0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine()); // 공백을 기준으로 토큰화해서 나누기
+        int[] numArr = new int[n];
+        int max = 0;
+        // 배열에 값 받으면서 가장 큰 값 찾기
         for (int i = 0; i < n; i++) {
-            numArr[i] = scanner.nextInt();
-            if(max<numArr[i]){
-                max = numArr[i];
+            int num = Integer.parseInt(st.nextToken());
+            numArr[i] = num;
+            if(max < num){
+                max = num;
             }
         }
+
+        // 조작한 점수의 평균 구하기
         double sum = 0;
         for (int i = 0; i < n; i++) {
-            sum += ((double)numArr[i]/(double)max)*100;
+            sum += (double)numArr[i] / max * 100;
         }
-        double result = sum/n;
-        System.out.println(result);
+        System.out.println(sum/n);
     }
 }
