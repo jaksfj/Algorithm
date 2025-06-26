f9614fbc4775af9845b39233f7eb6d5bc9ee1f6a
김민재 <64482590+jaksfj@users.noreply.github.com>
2025-06-27 00:29:05 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void)
+{// hint: \"는 "이고 \\는 \이다
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\\n");
+    printf("|\"^\"`    |\n");
+    printf("||_/=\\\\__|\n");
+    return 0;
+}
