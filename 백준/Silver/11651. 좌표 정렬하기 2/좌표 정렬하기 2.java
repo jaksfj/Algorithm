975e4af349149d413e16596fe162780110b41527
김민재 <64482590+jaksfj@users.noreply.github.com>
2025-07-28 22:18:13 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1232 ms, Memory: 61164 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,42 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main { // 좌표 정렬하기2
+    static class XY implements Comparable<XY>{
+        int x;
+        int y;
+        XY(int x,int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(XY o){
+            if(this.y==o.y){
+                return this.x-o.x;
+            }
+            return this.y-o.y;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        XY[] arr = new XY[n];
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arr[i] = new XY(x,y);
+        }
+        Arrays.sort(arr);
+        for(XY e : arr){
+            System.out.println(e.x+" "+e.y);
+        }
+    }
+}
