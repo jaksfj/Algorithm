65e4f96a17ebe54f1d5a7f5ba8f5e178b516ba0c
김민재 <64482590+jaksfj@users.noreply.github.com>
2025-08-21 17:15:47 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2116 ms, Memory: 533352 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -1,38 +1,50 @@
+
+
 import java.io.*;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException{
+public class Main { // 최솟값 찾기
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        ArrayDeque<Node> deque = new ArrayDeque<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken()); // 수의 개수
-        int l = Integer.parseInt(st.nextToken()); // 윈도우 크기
-        ArrayDeque<Node> window = new ArrayDeque<>();
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        for(int i = 1; i <= n; i++){
-            int nextNode = Integer.parseInt(st.nextToken());
-            while(!window.isEmpty() && window.getLast().value >= nextNode) { // 윈도우의 last 쪽 요소가 들어가는 노드의 값보다 작아야 한다.
-                // 윈도우가 비어있지 않은 상태일 때 진행해야한다.
-                window.removeLast();
-            }
-
-            window.offer(new Node(i,nextNode));
-
-            if (window.getLast().index - window.getFirst().index >= l) { // 윈도우의 크기를 초과하면 안되므로 앞 요소 삭제
-                window.removeFirst();
-            }
-
-            bw.write(window.getFirst().value + " ");
+        int[] arr = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+
+        deque.offer(new Node(1,arr[1]));
+        bw.write(deque.peekFirst().value + " ");
+        int index = 2;
+        while(!deque.isEmpty() && index <= n){
+            while(!deque.isEmpty() && deque.peekLast().value>=arr[index]){ // 만약 들어갈 자리의 수보다 크면 제거
+                deque.pollLast();
+            }
+            deque.offer(new Node(index,arr[index]));
+            if(index-deque.peekFirst().index>=l){
+                deque.pollFirst();
+            }
+            bw.write(deque.peekFirst().value + " ");
+            index++;
+        }
+
         bw.flush();
+        bw.close();
+        br.close();
     }
 
-    private static class Node{
+    static class Node {
         int index;
         int value;
-        Node(int index, int value){
+
+        Node(int index,int value){
             this.index = index;
             this.value = value;
         }
