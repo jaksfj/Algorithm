4e62c38444db0e1076a8bc6dadbeab6703c4ee62
김민재 <64482590+jaksfj@users.noreply.github.com>
2025-09-10 15:33:58 +0900

    [D2] Title: 간단한 소인수분해, Time: 103 ms, Memory: 27,008 KB -BaekjoonHub

---
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
@@ -0,0 +1,68 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();	
+        int[] numbers = {11,7,5,3,2};
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            int[] answer = new int[5];
+        	for(int i = 0;i<5;i++){
+        		answer[i] = 0; 
+        	}	
+			int n = sc.nextInt();
+            for(int i = 0;i<5;i++){
+                if(n==0){
+                	break;
+                }
+            	while(n%numbers[i]==0 && n!=0){
+                    n/=numbers[i]; // n값 갱신
+                    answer[i]++;
+                }
+            }
+            System.out.print("#" + test_case + " ");
+            for(int i = 4;i>=0;i--){
+               	System.out.print(answer[i] + " ");
+            }
+            System.out.println();
+		}
+	}
+}
