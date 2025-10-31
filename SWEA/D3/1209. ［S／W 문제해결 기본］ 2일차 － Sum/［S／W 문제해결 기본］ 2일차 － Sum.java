66efd3cf61b4fc75374386090c8a428cda41b6c7
김민재 <myki011122@gmail.com>
2025-10-31 12:49:40 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 143 ms, Memory: 38,104 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -1,75 +1,52 @@
 import java.util.Scanner;
 import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Solution
 {
 	public static void main(String args[]) throws Exception
     {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         Scanner sc = new Scanner(System.in);
         int T;
         T=10;
 
         for(int test_case = 1; test_case <= T; test_case++)
         {
-            int n = sc.nextInt();
+            int n = Integer.parseInt(br.readLine());
             int[][] arr = new int[100][100];
             for (int i = 0; i < 100; i++) {
+                st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < 100; j++) {
-                    arr[i][j] = sc.nextInt();
+                    arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
             // 1) 가로 세로 탐색
             int max = 0;
+            int upDiagonalSum = 0; // 오른쪽 위 대각선
+            int downDiagonalSum = 0; // 오른쪽 아래 대각선
             for (int i = 0; i < 100; i++) {
+                int rowSum = 0;
+                int columnSum = 0;
                 for (int j = 0; j < 100; j++) {
-                    // 가로
-                    int sum = 0;
-                    for (int k = 0; k < 100; k++) {
-                        sum += arr[i][k];
-                    }
-                    if(max<sum){
-                        max = sum;
-                    }
-                    // 세로
-                    sum = 0;
-                    for (int k = 0; k < 100; k++) {
-                        sum += arr[k][j];
-                    }
-                    if(max<sum){
-                        max = sum;
-                    }
+                    // 가로 세로 합 구하기
+                    rowSum += arr[i][j];
+                    columnSum += arr[j][i];
                 }
-            }
+                // 가로, 세로
+                max = Math.max(max,columnSum);
+                max = Math.max(max,rowSum);
 
-            // 2) 대각선 탐색
-            // 오른쪽 아래 대각선
-            int sum = 0;
-            // i: 99~0, j: 0~100
-            int i = 99;
-            int j = 0;
-            while(i>=0 && j<100){
-                sum += arr[i][j];
-                i--;
-                j++;
+                // 오른쪽 위,아래 대각선
+                upDiagonalSum += arr[i][i];
+                downDiagonalSum += arr[99-i][i];
             }
-            if(max<sum){
-                max = sum;
-            }
-
-            // 오른쪽 위 대각선
-            sum = 0;
-            // i: 0~100, j: 0~100
-            i = 0;
-            j = 0;
-            while(i<100 && j<100){
-                sum += arr[i][j];
-                i++;
-                j++;
-            }
-            if(max<sum){
-                max = sum;
-            }
+            max = Math.max(upDiagonalSum,max);
+            max = Math.max(downDiagonalSum,max);
 
             System.out.println("#" + n + " " + max);
         }
