b870079697aa48eb8d4bce81f53e5a28a6c030cf
김민재 <myki011122@gmail.com>
2025-11-03 17:20:58 +0900

    [D3] Title: [S/W 문제해결 기본] 10일차 - 비밀번호, Time: 170 ms, Memory: 27,136 KB -BaekjoonHub

--- SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.java
+++ SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.java
@@ -1,58 +1,31 @@
 import java.util.Scanner;
 import java.io.FileInputStream;
-import java.util.StringTokenizer;
+import java.util.ArrayDeque;
 
 class Solution
 {
-	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringTokenizer st;
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
 
-        for(int test_case = 1; test_case <= 10; test_case++)
-        {
-            st = new StringTokenizer(sc.nextLine());
-            int n = Integer.parseInt(st.nextToken());
-            String str = st.nextToken();
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(str.substring(i,i+1));
-            }
-
-            int prev = -1;
-            int cur;
-            for (int i = 0; i < n; i++) {
-                cur = arr[i];
-                if(prev==cur){
-                    // 투포인터로 넓혀가기
-                    int start = i-1;
-                    int end = i;
-                    while(start>=0 && end<n){
-                        while(arr[start]==-1 && start>0){
-                            start--;
-                        }
-                        while(arr[end]==-1 && end<n-1){
-                            end++;
-                        }
-                        if(arr[start]==arr[end]){ // 둘이 같으면 -1로 설정
-                            arr[start] = -1;
-                            arr[end] = -1;
-                        }else{
-                            break;
-                        }
-                        start--;
-                        end++;
-                    }
+		for(int test_case = 1; test_case <= 10; test_case++)
+		{
+            int n = sc.nextInt();
+            String s = sc.next();
+            ArrayDeque<Character> dq = new ArrayDeque<>();
+            for(int i = 0;i<n;i++){
+            	char c = s.charAt(i);
+                if(!dq.isEmpty() && dq.peekLast() == c){
+                	dq.pollLast();
+                }else{
+                	dq.addLast(c);
                 }
-                prev = cur;
             }
-
             System.out.print("#" + test_case + " ");
-            for (int i = 0; i < n; i++) {
-                if(arr[i]!=-1){
-                    System.out.print(arr[i]);
-                }
+            for(char c : dq){
+            	System.out.print(c);
             }
             System.out.println();
-        }
-    }
+		}
+	}
 }
