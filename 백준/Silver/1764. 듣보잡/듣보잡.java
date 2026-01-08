3fdbb525d43560383bf5c9ce7471c10863b63bbd
김민재 <myki011122@gmail.com>
2026-01-08 16:08:41 +0900

    [Silver IV] Title: 듣보잡, Time: 276 ms, Memory: 26908 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        Set<String> set = new HashSet<>();
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+        for (int i = 0; i < m; i++) {
+            String str = br.readLine();
+            if(set.contains(str)){
+                list.add(str);
+            }
+        }
+        Collections.sort(list);
+        bw.write(list.size() + "\n");
+        for(String str : list){
+            bw.write(str + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
