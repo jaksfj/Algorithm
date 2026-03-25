e938afbd356b4afd8624b8e0004500bc40b0235c
김민재 <myki011122@gmail.com>
2026-03-25 15:28:53 +0900

    [Silver IV] Title: 크림 파스타, Time: 564 ms, Memory: 52996 KB -BaekjoonHub

---
+++ 백준/Silver/25214. 크림 파스타/크림 파스타.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long min = 0;
+		long[] dp = new long[n+1];
+		int index = 1;
+		while(st.hasMoreTokens()) {
+			long num = Long.parseLong(st.nextToken());
+			if(index==1) {
+				bw.write(dp[index] + " ");
+				dp[index] = 0;
+				min = num;
+				index++;
+				continue;
+			}
+			if(num<min) { // 최소값 갱신
+				min = num;
+			}
+			dp[index] = Math.max(dp[index-1], Math.abs(num-min));
+			bw.write(dp[index] + " ");
+			index++;
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
