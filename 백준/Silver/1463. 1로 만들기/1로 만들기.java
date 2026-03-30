d6f225a0f47284556b915e6dc797d12a01255b04
김민재 <myki011122@gmail.com>
2026-03-30 22:41:38 +0900

    [Silver III] Title: 1로 만들기, Time: 188 ms, Memory: 21612 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,22 +1,23 @@
-import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] dp = new int[n+1];
-        dp[1] = 0;
-        for (int i = 2; i <= n; i++) {
-            int min = dp[i-1]; // 1을 뺀 수가 완성되기까지의 횟수가 기준
-            if(i%2==0){
-                min = Math.min(dp[i/2],min); // 2로 나눠진 수가 완성되기까지의 횟수
-            }
-            if(i%3==0){
-                min = Math.min(dp[i/3],min); // 3로 나눠진 수가 완성되기까지의 횟수
-            }
-            dp[i] = min+1; // 최소 한번의 연산은 더해야 i가 될수있음
-        }
-        System.out.println(dp[n]);
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int[] dp = new int[x+1];
+		for(int i = 1;i<=x;i++) {
+			// 2로 나눠지면 2로 나누고, 3으로 나눠지면 3으로 나누고, 아니면 1을 뺴라
+			// 그렇게 해서 1을 만들 수 있는 연산 횟수의 최솟값
+			if(i%2==0 && i%3==0) {
+				dp[i] = Math.min(dp[i/3],dp[i/2])+1;
+			}else if(i%2==0) {
+				dp[i] = Math.min(dp[i-1], dp[i/2])+1;
+			}else if(i%3==0) {
+				dp[i] = Math.min(dp[i-1],dp[i/3])+1;
+			}else if(i>1){
+				dp[i] = dp[i-1]+1;
+			}
+		}
+		System.out.print(dp[x]);
+	}
 }
