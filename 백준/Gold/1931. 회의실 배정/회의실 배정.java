055d6ffc5b4a6ca5bef62ba52c72eeb8f7b914df
김민재 <myki011122@gmail.com>
2026-04-01 01:53:09 +0900

    [Gold V] Title: 회의실 배정, Time: 540 ms, Memory: 45908 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,52 +1,49 @@
-import java.io.*;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); // 회의 개수
-        PriorityQueue<Node> pq = new PriorityQueue<Node>(new Comparator<Node>() {
-            @Override
-            public int compare(Node o1, Node o2) {
-                if(o1.endTime==o2.endTime){ // 종료시간이 같으면 시작시간이 빠른 순으로 정렬
-                    return o1.startTime-o2.startTime;
-                }else{ // 종료시간이 다르면
-                    return o1.endTime-o2.endTime; // 종료시간이 빠른 순으로 정렬
-                    // 만약 (2,2) 와 (1,2) 가 존재한다면 종료시간이 같지만 (1,2)가 불가능할 수 있기 때문이다.
-                    // (1,2) 가 먼저 나오고 (2,2) 가 나오도록 하는 것!
-                }
-            }
-        });
-
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int startTime = Integer.parseInt(st.nextToken());
-            int endTime = Integer.parseInt(st.nextToken());
-            pq.offer(new Node(startTime,endTime));
-        }
-
-        int cnt = 0;
-        int end = -1;
-        while(!pq.isEmpty()){
-            Node next = pq.poll();
-            if(next.startTime >= end){
-                cnt++;
-                end = next.endTime; // 종료시간 갱신
-            }
-        }
-
-        System.out.println(cnt);
-    }
-
-    static class Node{
-        int startTime;
-        int endTime;
-
-        Node(int startTime, int endTime){
-            this.startTime = startTime;
-            this.endTime = endTime;
-        }
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		ArrayList<Time> list = new ArrayList<>();
+		for(int i = 0;i<n;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			list.add(new Time(start,end));
+		}
+		int answer = 1; // 회의 하나 있을 경우
+		Collections.sort(list);
+		int curIndex = 0;
+		for(int i = 1;i<list.size();i++) {
+			if(list.get(curIndex).endTime<=list.get(i).startTime) {
+				answer++;
+				curIndex = i;
+			}
+		}
+		System.out.print(answer);
+	}
+	
+	static class Time implements Comparable<Time>{
+		int startTime;
+		int endTime;
+		Time(int startTime, int endTime){
+			this.startTime = startTime;
+			this.endTime = endTime;
+		}
+		
+		@Override
+		public int compareTo(Time o) {
+			if(this.endTime==o.endTime) {
+				return this.startTime-o.startTime;
+			}
+			return this.endTime-o.endTime;
+		}
+	}
 }
